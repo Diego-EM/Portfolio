e8bc4ff1a21e7a1248c8acc267561379e0f6937f
Diego-EM <dmunozaguilar2000@gmail.com>
2022-05-03 08:52:28 -0500

    Message contact button added

--- scripts/email.js
+++ scripts/email.js
@@ -7,9 +7,9 @@ contactForm.addEventListener('submit', function(e){
     const validField = /[^\s].*/i;
     const validEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-    validateField(formContent.name, validField, 'name', 'You should type your name to meet you :)')
+    validateField(formContent.name, validField, 'name', 'You should type your name to meet you')
         .then(() => validateField(formContent.email, validEmail, 'email', 'You must type a valid email address'))
-        .then(() => validateField(formContent.body, validField, 'body', 'You can write your idea here :)'))
+        .then(() => validateField(formContent.body, validField, 'body', 'You can write your idea here'))
         .then(() => {
             switchLoader();
             const email = {
